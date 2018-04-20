5aed473a5c5992669a336da0e738212e893a9a1b
BeardOfDoom <vecsi96@gmail.com>
2018-04-20 21:53:25 +0200

    Added new Node class for backtrack algorithm with path length limitation

---
+++ MIgyak9/src/threejugs/NodeWithDepth.java
@@ -0,0 +1,65 @@
+package threejugs;
+
+import java.util.List;
+
+public class NodeWithDepth {
+
+	State state;
+	Node parent;
+	Operator operator;
+	List<Operator> triedOperators;
+	int depth;
+	
+	public NodeWithDepth(State state, Node parent, Operator operator, List<Operator> triedOperators, int depth) {
+		this.state = state;
+		this.parent = parent;
+		this.operator = operator;
+		this.triedOperators = triedOperators;
+		this.depth = depth;
+	}
+	
+	public State getState() {
+		return state;
+	}
+	
+	public void setState(State state) {
+		this.state = state;
+	}
+	
+	public Node getParent() {
+		return parent;
+	}
+	
+	public void setParent(Node parent) {
+		this.parent = parent;
+	}
+	
+	public Operator getOperator() {
+		return operator;
+	}
+	
+	public void setOperator(Operator operator) {
+		this.operator = operator;
+	}
+	
+	public List<Operator> getTriedOperators() {
+		return triedOperators;
+	}
+	
+	public void setTriedOperators(List<Operator> triedOperators) {
+		this.triedOperators = triedOperators;
+	}
+	
+	public int getDepth() {
+		return depth;
+	}
+	
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
+	
+	@Override
+	public String toString() {
+		return "Node [state=" + state + "]";
+	}
+}
