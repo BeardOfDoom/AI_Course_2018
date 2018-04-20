720ecce4b4da83a8de1b59c31604f1561a2d3b5c
BeardOfDoom <vecsi96@gmail.com>
2018-04-20 22:20:08 +0200

    Initialized 10th lesson

---
+++ MIgyak10/src/Main.java
@@ -0,0 +1,9 @@
+import threejugs.State;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] jugs = {5, 0, 0};
+		State start = new State(jugs);
+	}
+}
---
+++ MIgyak10/src/threejugs/Node.java
@@ -0,0 +1,56 @@
+package threejugs;
+
+import java.util.List;
+
+public class Node {
+
+	State state;
+	Node parent;
+	Operator operator;
+	List<Operator> triedOperators;
+	
+	public Node(State state, Node parent, Operator operator, List<Operator> triedOperators) {
+		this.state = state;
+		this.parent = parent;
+		this.operator = operator;
+		this.triedOperators = triedOperators;
+	}
+	
+	public State getState() {
+		return state;
+	}
+	
+	public void setState(State state) {
+		this.state = state;
+	}
+	
+	public Node getParent() {
+		return parent;
+	}
+	
+	public void setParent(Node parent) {
+		this.parent = parent;
+	}
+	
+	public Operator getOperator() {
+		return operator;
+	}
+	
+	public void setOperator(Operator operator) {
+		this.operator = operator;
+	}
+	
+	public List<Operator> getTriedOperators() {
+		return triedOperators;
+	}
+	
+	public void setTriedOperators(List<Operator> triedOperators) {
+		this.triedOperators = triedOperators;
+	}
+	
+	@Override
+	public String toString() {
+		return "Node [state=" + state + "]";
+	}
+	
+}
