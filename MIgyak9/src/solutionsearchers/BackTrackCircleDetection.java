4b3bf5ce5f2fb02359c40e10f046b7992f6c97bd
BeardOfDoom <vecsi96@gmail.com>
2018-04-20 21:48:06 +0200

    Implemented backtrack algorithm with circle detection

---
+++ MIgyak9/src/solutionsearchers/BackTrackCircleDetection.java
@@ -0,0 +1,62 @@
+package solutionsearchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import threejugs.Node;
+import threejugs.Operator;
+import threejugs.State;
+
+public class BackTrackCircleDetection {
+
+	Node actual;
+	List<State> reachedStates;
+
+	public BackTrackCircleDetection(Node start) {
+		actual = start;
+		reachedStates = new ArrayList<>();
+	}
+	
+	
+	public void search() {
+		while(true) {
+			if(actual == null)
+				break;
+			if(actual.getState().isGoal())
+				break;
+			if(reachedStates.contains(actual.getState()))
+				actual = actual.getParent();
+			
+			reachedStates.add(actual.getState());
+			
+			boolean didUseOperator = false;
+			
+			for(Operator operator : Operator.OPERATORS) {
+				if(operator.isApplicable(actual.getState()) && !actual.getTriedOperators().contains(operator)) {
+					actual.getTriedOperators().add(operator);
+					State newState = operator.apply(actual.getState());
+					actual = new Node(newState, actual, operator, new ArrayList<>());
+					didUseOperator = true;
+					System.out.println(newState);
+					
+					try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break;
+				}
+			}
+			
+			if(!didUseOperator)
+				actual = actual.getParent();
+		}
+		
+		if(actual != null) {
+			System.out.println(actual);
+		} else {
+			System.out.println("Nincs megoldás");
+		}
+	}
+}
